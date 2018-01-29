c44c7508c03bfa72f161145582d80835b976f27b
Amber Slovick <amber.slovick@gmail.com>
2018-01-29 11:34:35 -0800

    Added function to access data-name for hovered subdistricts

--- web/js/script.js
+++ web/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-	
+	$(".parks, .d1, .d2, .d3, .d4, .d5, .d6, .d7, .d8, .d9, .d10").hover(
+		function(){ // mousein
+			var sdname = $(this).data("name");
+			$("#dist-popup").text(sdname);
+		}, function(){ // mouseout
+
+		}
+	);
 });
